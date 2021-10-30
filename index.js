3a8f9f170dfd8e9306845543aa5d50e1d7a0bab5
Safin-Ahmed <safin.ahmed2000@gmail.com>
2021-10-30 14:50:44 +0600

    fixed issue of query in get method of loading single data

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const ObjectId = require('mongodb').ObjectId();
+const ObjectId = require('mongodb').ObjectId;
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 const app = express();
